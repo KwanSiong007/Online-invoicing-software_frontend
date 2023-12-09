4c6fff425fc44d01ed444ee99d42900dc792034a
KwanSiong007 <ksong329@gmail.com>
2023-12-09 11:35:37 +0800

    feat: created responsive navbar and routing by using MUI and React Router Dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 import "./App.css";
+import ResponsiveAppBar from "./components/Navbar";
+import Business from "./pages/Business";
+import Invoice from "./pages/Invoice";
+import Contacts from "./pages/Contacts";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const routes = [
+  { path: "/Business", element: <Business /> },
+  { path: "/Invoice", element: <Invoice /> },
+  { path: "/Contacts", element: <Contacts /> },
+];
 
 function App() {
-  return "Hi";
+  return (
+    <Router>
+      <ResponsiveAppBar />
+      <Routes>
+        {routes.map((route) => (
+          <Route key={route.path} path={route.path} element={route.element} />
+        ))}
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
