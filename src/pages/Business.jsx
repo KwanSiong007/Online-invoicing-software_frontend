58a6dca908794c7cc5d8612798cccc861619dd35
KwanSiong007 <ksong329@gmail.com>
2023-12-10 22:52:20 +0800

    feat: remove underline in button

--- src/pages/Business.jsx
+++ src/pages/Business.jsx
@@ -4,10 +4,17 @@ import { Button } from "@mui/material";
 import { Link } from "react-router-dom";
 
 function Business() {
+  const linkStyle = {
+    textDecoration: "none",
+    color: "inherit",
+  };
+
   return (
     <div style={{ margin: "10px 10px" }}>
       <Button variant="outlined">
-        <Link to={`/${"Invoice"}`}>{"New Invoice"}</Link>
+        <Link to="/Invoice" style={linkStyle}>
+          {"New Invoice"}
+        </Link>
       </Button>
     </div>
   );
