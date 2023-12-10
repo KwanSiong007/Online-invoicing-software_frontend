e3944a564a9b60fdb1ec40bfc2e60d77488af753
KwanSiong007 <ksong329@gmail.com>
2023-12-10 22:30:55 +0800

    feat: added "New invoice" button that used to redirect to Invoice page

--- src/pages/Business.jsx
+++ src/pages/Business.jsx
@@ -1,6 +1,15 @@
-import { Typography } from "@mui/material";
+// it has "New invoice" button
+// shows all, awaiting payment & paid invoice
+import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
 
 function Business() {
-  return <Typography>Business pg</Typography>;
+  return (
+    <div style={{ margin: "10px 10px" }}>
+      <Button variant="outlined">
+        <Link to={`/${"Invoice"}`}>{"New Invoice"}</Link>
+      </Button>
+    </div>
+  );
 }
 export default Business;
